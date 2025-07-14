91d31947f25e3758b212efc6dbdf2c48ea5ca3b5
“Helen” <25727036+helenzhou6@users.noreply.github.com>
2025-07-14 16:21:55 +0100

    Add dummy lora code to check it works

---
+++ test.py
@@ -0,0 +1,21 @@
+import torch 
+
+# Lora example
+class Base(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.proj = torch.nn.Linear(10, 5)
+        self.lora = torch.nn.Sequential(
+            torch.nn.Linear(10, 2, bias=False),
+            torch.nn.Dropout(0.1),
+            torch.nn.Linear(2, 5, bias=False)
+        )
+    
+    def forward(self, x):
+        x = self.proj(x)
+        return x + self.lora(x)
+
+base = Base()
+nums = sum(p.numel() for p in base.parameters())
+print('Params: ', nums)
+print(base)
