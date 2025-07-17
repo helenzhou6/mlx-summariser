e095eda1624554d6e29577f59b194870cb959c31
Ewan <ewanbeattie1@gmail.com>
2025-07-17 11:37:11 +0100

    Updating inference.py to use Lora weights

--- src/inference.py
+++ src/inference.py
@@ -4,14 +4,14 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import streamlit as st
 from peft import LoraConfig, get_peft_model, PeftModel
 
-MODEL_VERSION = 'v23'
+MODEL_VERSION = 'v0'
 
 @st.cache_resource(show_spinner=False)
 def load_models():
     init_wandb()
     device = get_device()
     
-    fine_tuned_path = load_artifact_path('qwenTLDRmodel', MODEL_VERSION)
+    fine_tuned_path = load_artifact_path('base_lora_weights_14', MODEL_VERSION)
 
     # Load the base Qwen model
     qwen_name = "Qwen/Qwen3-0.6B-Base"
@@ -22,29 +22,29 @@ def load_models():
     # Load the fine-tuned Lora model
 
     # TODO: Uncomment when the model has been saved as LoRa weights
-    # base_model = AutoModelForCausalLM.from_pretrained(qwen_name)
-    # fine_tuned_model = PeftModel.from_pretrained(base_model, fine_tuned_path)
-    # fine_tuned_model.to(device)
-    # fine_tuned_model.eval()
+    base_model = AutoModelForCausalLM.from_pretrained(qwen_name)
+    fine_tuned_model = PeftModel.from_pretrained(base_model, fine_tuned_path)
+    fine_tuned_model.to(device)
+    fine_tuned_model.eval()
 
     ### TEMPORARY
-    qwen_tokenizer = AutoTokenizer.from_pretrained(qwen_name, trust_remote_code=True, padding_side='left')
-    qwen_tokenizer.pad_token = qwen_tokenizer.eos_token
+    # qwen_tokenizer = AutoTokenizer.from_pretrained(qwen_name, trust_remote_code=True, padding_side='left')
+    # qwen_tokenizer.pad_token = qwen_tokenizer.eos_token
 
-    qwen_model = AutoModelForCausalLM.from_pretrained(qwen_name)
-    qwen_model.to(device)
-    qwen_model.config.pad_token_id = qwen_tokenizer.eos_token_id
-    lora_config = LoraConfig(
-        r=16, # rank - controls adapter size
-        lora_alpha=32,
-        target_modules=["q_proj", "v_proj", "k_proj", "o_proj"], # attention layers
-        lora_dropout=0.1,
-        bias="none",
-        task_type="CAUSAL_LM"
-    )
-    fine_tuned_model = get_peft_model(qwen_model, lora_config)
+    # qwen_model = AutoModelForCausalLM.from_pretrained(qwen_name)
+    # qwen_model.to(device)
+    # qwen_model.config.pad_token_id = qwen_tokenizer.eos_token_id
+    # lora_config = LoraConfig(
+    #     r=16, # rank - controls adapter size
+    #     lora_alpha=32,
+    #     target_modules=["q_proj", "v_proj", "k_proj", "o_proj"], # attention layers
+    #     lora_dropout=0.1,
+    #     bias="none",
+    #     task_type="CAUSAL_LM"
+    # )
+    # fine_tuned_model = get_peft_model(qwen_model, lora_config)
 
-    fine_tuned_model.load_state_dict(torch.load(fine_tuned_path, map_location=device))
+    # fine_tuned_model.load_state_dict(torch.load(fine_tuned_path, map_location=device))
     ###TEMPORARY
 
     tokenizer = AutoTokenizer.from_pretrained(qwen_name)
